2fc5d685ba54beb2dff6bd55893e0d6ffff08155
Sawyer1860 <sawyer1860@gmail.com>
2023-02-23 13:28:03 -0500

    Files created when running program on mac

--- lib/personal.dart
+++ lib/personal.dart
@@ -1,22 +1,20 @@
 import 'dart:html';
 
-class personal(Weight , Height) {
-    String personalWeight;
-    String personalHeight;
-    personal(personalWeight, personalHeight)
-    int bmi = personalWeight / personalHeight;
+class Personal {
+    double PersonalWeight;
+    double PersonalHeight;
+    Personal(this.PersonalWeight,this.PersonalHeight);
+
 }
 
-var personal_sample {
-  personal("155.5" , "70"), // sample of personal data that is correct
+var personal_sample = [
+  Personal(155.5 , 70), // sample of personal data that is correct
 
-  personal("130", "6 foot 5 inches"), // sample of personal data that is incorrect by including feeet and inches
+  Personal(130, 6), // sample of personal data that is incorrect by including feeet and inches
 
-  personal("", "60"), // sample of personal data that is imcomplete
+  Personal(5.5, 60), // sample of personal data that is imcomplete
 
-  personal("145", ""), //sample of personal data that is incomplete
+  Personal(45, 0), //sample of personal data that is incomplete
 
-  personal("", ""), //Sample of data with both fields that are incomplete
-
-  personal("145", "71") //Sample of Data is that is complete and correct (should debate whether real or int )
-}
+  Personal(5, 5), //Sample of data with both fields that are incomplete
+];
