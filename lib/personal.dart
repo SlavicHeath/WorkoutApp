5e80eceb5948f3fa1be722766b3e19085501c8d1
Sawyer1860 <sawyer1860@gmail.com>
2023-02-22 19:16:13 -0500

    Created the two files for personal and user classes, will edit and begin to implement in the following week
    Added two new files for personal and user

---
+++ lib/personal.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+
+class personal(Weight , Height) {
+    String personalWeight;
+    String personalHeight;
+    personal(personalWeight, personalHeight)
+    int bmi = personalWeight / personalHeight;
+}
---
+++ lib/user.dart
@@ -0,0 +1,4 @@
+class user(email, password) { 
+  String email;
+  String password; 
+}
