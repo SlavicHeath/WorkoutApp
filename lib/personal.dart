69d831735cd0238dd71337d99e991e516877e94a
Slavic Heath <mvh2069@uncw.edu>
2023-02-23 13:23:50 -0500

    personal fix

--- lib/personal.dart
+++ lib/personal.dart
@@ -1,22 +1,23 @@
 import 'dart:html';
 
-class personal(Weight , Height) {
-    String personalWeight;
-    String personalHeight;
-    personal(personalWeight, personalHeight)
-    int bmi = personalWeight / personalHeight;
+class personal {
+  double personalWeight;
+  double personalHeight;
+  personal(this.personalWeight, this.personalHeight);
 }
 
-var personal_sample {
-  personal("155.5" , "70"), // sample of personal data that is correct
+var personal_sample = [
+  personal(155.5, 70), // sample of personal data that is correct
 
-  personal("130", "6 foot 5 inches"), // sample of personal data that is incorrect by including feeet and inches
+  personal(130,
+      65), // sample of personal data that is incorrect by including feeet and inches
 
-  personal("", "60"), // sample of personal data that is imcomplete
+  personal(0, 60), // sample of personal data that is imcomplete
 
-  personal("145", ""), //sample of personal data that is incomplete
+  personal(145, 0), //sample of personal data that is incomplete
 
-  personal("", ""), //Sample of data with both fields that are incomplete
+  personal(0, 0), //Sample of data with both fields that are incomplete
 
-  personal("145", "71") //Sample of Data is that is complete and correct (should debate whether real or int )
-}
+  personal(145,
+      71) //Sample of Data is that is complete and correct (should debate whether real or int )
+];
