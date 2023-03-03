867b8c41864a84bba116fe38fa99c1e4b2492b8f
Sawyer1860 <sawyer1860@gmail.com>
2023-03-02 21:47:38 -0500

    Update personal.dart
    Not fully functioning yet, but will create a new screen called personal that will ask the user for their weight and height and then will proceed to calculate their BMI and return to them before they can continue.

--- lib/personal.dart
+++ lib/personal.dart
@@ -1,5 +1,8 @@
 import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 class Personal {
     double PersonalWeight;
     double PersonalHeight;
@@ -18,3 +21,35 @@ var personal_sample = [
 
   Personal(5, 5), //Sample of data with both fields that are incomplete
 ];
+
+class PersonalPage extends StatefulWidget {
+  @override
+  NewPersonalPageState createState() => NewPersonalPageState();
+}
+
+class NewPersonalPageState extends State<NewPersonalPage> {
+  final formKey = GlobalKey<FormState>();
+  final weightController = TextEditingController();
+  final heightController = TextEditingController();
+}
+
+void _submit() async  {
+    if (formKey.currentState!.validate()) {
+    final weight = double.parse(weightController.text);
+    final height = double.parse(heightController.text);
+    final bmi = weight / (weight * height);
+
+
+
+
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        title: Text('BMI Calculation');
+        content: Text('After entering your height and weight your BMI is ${NumberFormat('0.0').format(bmi)}'),
+      ))
+  }
+}
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
