a25f5b8867d5bb7c2ec5a650ddddf3151307689b
Sawyer1860 <sawyer1860@gmail.com>
2023-03-12 21:55:37 -0400

    creates a button for personal information and allows you to input height and weight for BMI

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:workoutpet/personal.dart';
 
 void main() {
-  runApp(MaterialApp(title: "WorkoutPet", home: HomeScreen()));
+  runApp(const MaterialApp(title: "WorkoutPet", home: HomeScreen()));
 }
 
 class HomeScreen extends StatelessWidget {
+    const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +14,37 @@ class HomeScreen extends StatelessWidget {
         title: const Text("WorkoutPet"),
         backgroundColor: Colors.purple,
       ),
-      body: Column(),
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        // decoration: const BoxDecoration(
+        //   image: DecorationImage(
+        //       image: AssetImage(""), fit: BoxFit.cover),
+        // ),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            // Create button
+            ElevatedButton(
+              onPressed: () {
+                // Pushes on a stack for back arrow button to form
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      //The right side is the widget you want to go to
+                      builder: (context) => const PersonalInfoPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(200, 40),
+                backgroundColor: Colors.purple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: const Text("Personal Information"),
+            ),
+         ]),
+        )
+      )
     );
   }
 }
--- lib/personal.dart
+++ lib/personal.dart
@@ -1,55 +1,123 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class Personal {
-    double PersonalWeight;
-    double PersonalHeight;
-    Personal(this.PersonalWeight,this.PersonalHeight);
+//class Personal {
+ //   double PersonalWeight;
+  //  double PersonalHeight;
+  //  Personal(this.PersonalWeight,this.PersonalHeight);
 
-}
+//}
 
-var personal_sample = [
-  Personal(155.5 , 70), // sample of personal data that is correct
+//var personal_sample = [
+  //Personal(155.5 , 70), // sample of personal data that is correct
 
-  Personal(130, 6), // sample of personal data that is incorrect by including feeet and inches
+  //Personal(130, 6), // sample of personal data that is incorrect by including feeet and inches
 
-  Personal(5.5, 60), // sample of personal data that is imcomplete
+ // Personal(5.5, 60), // sample of personal data that is imcomplete
 
-  Personal(45, 0), //sample of personal data that is incomplete
+ // Personal(45, 0), //sample of personal data that is incomplete
 
-  Personal(5, 5), //Sample of data with both fields that are incomplete
-];
+  //Personal(5, 5), //Sample of data with both fields that are incomplete
+// ];
 
-class PersonalPage extends StatefulWidget {
+
+
+class PersonalInfoPage extends StatefulWidget {
+  const PersonalInfoPage({super.key});
   @override
-  NewPersonalPageState createState() => NewPersonalPageState();
+  // ignore: library_private_types_in_public_api
+  _PersonalInfoPageState createState() => _PersonalInfoPageState();
 }
 
-class NewPersonalPageState extends State<NewPersonalPage> {
-  final formKey = GlobalKey<FormState>();
-  final weightController = TextEditingController();
-  final heightController = TextEditingController();
-}
+class _PersonalInfoPageState extends State<PersonalInfoPage> {
+  final _formKey = GlobalKey<FormState>();
+  // ignore: prefer_final_fields
+  TextEditingController _weightController = TextEditingController();
+  // ignore: prefer_final_fields
+  TextEditingController _heightController = TextEditingController();
+  double _bmiResult = 0.0;
 
-void _submit() async  {
-    if (formKey.currentState!.validate()) {
-    final weight = double.parse(weightController.text);
-    final height = double.parse(heightController.text);
-    final bmi = weight / (weight * height);
+  void _calculateBMI() {
+    double weight = double.tryParse(_weightController.text) ?? 0.0;
+    double height = double.tryParse(_heightController.text) ?? 0.0;
 
+    if (weight <= 0 || height <= 0) {
+      setState(() {
+        _bmiResult = 0.0;
+      });
+      return;
+    }
 
-
-
-    showDialog(
-      context: context,
-      builder: (context) => const AlertDialog(
-        title: Text('BMI Calculation');
-        content: Text('After entering your height and weight your BMI is ${NumberFormat('0.0').format(bmi)}'),
-      ))
+    double bmi = (weight * 703) / (height * height);
+    setState(() {
+      _bmiResult = bmi;
+    });
   }
-}
-@override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Information'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _weightController,
+                decoration: const InputDecoration(
+                  labelText: 'Weight (lbs)',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your weight';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _heightController,
+                decoration: const InputDecoration(
+                  labelText: 'Height (inch)',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your height';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _calculateBMI();
+                  }
+                },
+                // ignore: prefer_const_constructors
+                child: Text('Calculate BMI'),
+              ),
+              // ignore: prefer_const_constructors
+              SizedBox(height: 16.0),
+              Text(
+                _bmiResult == 0.0
+                    ? 'Please enter your weight and height'
+                    : 'Your BMI is ${_bmiResult.toStringAsFixed(1)}',
+                // ignore: prefer_const_constructors
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
