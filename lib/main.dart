9d96d8a1a731d6f8ae8572b11b17e1ed33e0fe9e
Slavic Heath <mvh2069@uncw.edu>
2023-03-14 13:37:44 -0400

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:workoutpet/personal.dart';
 import 'package:workoutpet/sign_in.dart';
 import 'package:workoutpet/signup.dart';
+import 'package:workoutpet/workout.dart';
 import 'battle.dart';
 import 'firebase_options.dart';
 
@@ -75,7 +76,7 @@ class HomeScreen extends StatelessWidget {
                   Navigator.of(context).push(
                     MaterialPageRoute(
                         //The right side is the widget you want to go to
-                        builder: (context) => PersonalInfoPage()),
+                        builder: (context) => LoginScreen()),
                   );
                 },
                 style: ElevatedButton.styleFrom(
@@ -86,6 +87,23 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 child: Text("Personal"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        //The right side is the widget you want to go to
+                        builder: (context) => WorkoutPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(200, 40),
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text("Workout"),
               ),
             ]),
           ),
