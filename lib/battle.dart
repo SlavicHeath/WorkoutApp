a1eae982947c02e3f7b6b75ee39f53045dbc4032
Slavic Heath <mvh2069@uncw.edu>
2023-03-13 22:48:37 -0400

    Revert "Merge pull request #19 from UNCW-CSC-450/OpponentClass"
    This reverts commit f18a65a973fd93dd8141d299d7293f2a3f2fc0ed, reversing
    changes made to 6fc3d9cb8bf45b2fcda0fb6b7e58669719e938e9.
    
    	modified:   lib/battle.dart
    	deleted:    lib/bot.dart
    
    Changes not staged for commit:
    	modified:   lib/sign_in.dart

--- lib/battle.dart
+++ lib/battle.dart
@@ -1,25 +1,17 @@
 //Ben Williams
 
-import 'character.dart';
-import 'bot.dart';
-
 //Placeholder until character class is fully complete
 //(will pass in a character object and use .get()s to calculate health, strenth , etc...)
 class Battle {
-  var userCharacter;
-  var botCharacter;
-  int userCurHealth = 0;
-  int botCurHealth = 0;
+  int? UserHealth;
+  int? UserStrength;
+  int? BotHealth;
+  int? BotStrength;
 
   //contructor
-  Battle(this.userCharacter) {
-    userCurHealth = userCharacter.health;
-    botCharacter = Bot(userCharacter.health, userCharacter.strength);
-    botCurHealth = botCharacter.botHealth;
-  }
+  Battle(this.UserHealth, this.UserStrength, this.BotHealth, this.BotStrength);
 }
 
-/* battle examples do not work until character incorporates stats (health, strength, etc...)
 var battle = [
   //complete constructor
   Battle(2000, 100, 1500, 125),
@@ -29,4 +21,4 @@ var battle = [
   Battle(5000, 35, 0, 0),
   //All data missing
   Battle(0, 0, 0, 0)
-];*/
+];
