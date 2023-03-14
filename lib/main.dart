f670cd9589ed7e93fb884df0ed803f98f7eb0ef5
Slavic Heath <mvh2069@uncw.edu>
2023-03-14 11:17:07 -0400

    modified:   lib/main.dart 	modified:   lib/personal.dart 	modified:   lib/sign_in.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:workoutpet/sign_in.dart';
 import 'package:workoutpet/signup.dart';
@@ -18,58 +19,58 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("WorkoutPet"),
-        backgroundColor: Colors.purple,
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(8),
-        // decoration: const BoxDecoration(
-        //   image: DecorationImage(
-        //       image: AssetImage(""), fit: BoxFit.cover),
-        // ),
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            // Create button
-            ElevatedButton(
-              onPressed: () {
-                // Pushes on a stack for back arrow button to form
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      //The right side is the widget you want to go to
-                      builder: (context) => LoginScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(200, 40),
-                backgroundColor: Colors.purple,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              child: Text("LogIn"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      //The right side is the widget you want to go to
-                      builder: (context) => SignUpScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(200, 40),
-                backgroundColor: Colors.purple,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              child: Text("SignUp"),
-            ),
-          ]),
+        appBar: AppBar(
+          title: const Text("WorkoutPet"),
+          backgroundColor: Colors.purple,
         ),
-      )
-    );
+        body: Container(
+          padding: const EdgeInsets.all(8),
+          // decoration: const BoxDecoration(
+          //   image: DecorationImage(
+          //       image: AssetImage(""), fit: BoxFit.cover),
+          // ),
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              // Create button
+              ElevatedButton(
+                onPressed: () {
+                  // Pushes on a stack for back arrow button to form
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        //The right side is the widget you want to go to
+                        builder: (context) => LoginScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(200, 40),
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text("LogIn"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        //The right side is the widget you want to go to
+                        builder: (context) => SignUpScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(200, 40),
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text("SignUp"),
+              ),
+            ]),
+          ),
+        ));
   }
 }
--- lib/personal.dart
+++ lib/personal.dart
@@ -1,27 +1,6 @@
 import 'dart:html';
 import 'package:flutter/material.dart';
 
-//class Personal {
- //   double PersonalWeight;
-  //  double PersonalHeight;
-  //  Personal(this.PersonalWeight,this.PersonalHeight);
-
-//}
-
-//var personal_sample = [
-  //Personal(155.5 , 70), // sample of personal data that is correct
-
-  //Personal(130, 6), // sample of personal data that is incorrect by including feeet and inches
-
- // Personal(5.5, 60), // sample of personal data that is imcomplete
-
- // Personal(45, 0), //sample of personal data that is incomplete
-
-  //Personal(5, 5), //Sample of data with both fields that are incomplete
-// ];
-
-
-
 class PersonalInfoPage extends StatefulWidget {
   const PersonalInfoPage({super.key});
   @override
@@ -121,6 +100,7 @@ class _PersonalInfoPageState extends State<PersonalInfoPage> {
     );
   }
 }
+
 class personal {
   double personalWeight;
   double personalHeight;
