13a88ffb16acc71c758ac89b785805699f8b6880
Slavic Heath <mvh2069@uncw.edu>
2023-04-04 12:38:26 -0400

    Model viewer demo

---
+++ lib/character_select.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+class Character extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Model Viewer")),
+        body: ModelViewer(
+          src: 'assets/character/Astronaut.glb',
+          alt: "A 3D model of an astronaut",
+          ar: true,
+          autoRotate: true,
+          cameraControls: true,
+        ),
+      ),
+    );
+  }
+}
