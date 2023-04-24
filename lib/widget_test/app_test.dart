6ae6e3649637ac914112fdb3687343c32c61d330
Slavic Heath <mvh2069@uncw.edu>
2023-04-24 00:41:28 -0400

    Fix the merge errors

--- lib/widget_test/app_test.dart
+++ lib/widget_test/app_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 import 'package:workoutpet/main.dart' as app;
 import 'package:workoutpet/sign_in.dart';
