394d4c5c1be65b86c9d2931853fd87c4c063d6be
Slavic Heath <mvh2069@uncw.edu>
2023-04-24 00:34:40 -0400

    Fix the merge errors

--- lib/personal.dart
+++ lib/personal.dart
@@ -1,9 +1,8 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:workoutpet/battle.dart';
+import 'package:workoutpet/sign_in.dart';
 import 'package:workoutpet/workout.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -24,9 +23,6 @@ class PersonalInfoPage extends StatefulWidget {
   // ignore: library_private_types_in_public_api
   _PersonalInfoPageState createState() => _PersonalInfoPageState();
 }
-
-
-
 
 ///
 /// [_PersonalInfoPageState.]
@@ -60,36 +56,13 @@ class _PersonalInfoPageState extends State<PersonalInfoPage> {
       bmiResult = bmi;
     });
   }
-///
-/// [@var		final	authUser]
-/// [@global]
-///
-final authUser = FirebaseAuth.instance.currentUser;
 
   final authUser = FirebaseAuth.instance.currentUser;
-
   final personalInfoRef = FirebaseFirestore.instance.collection('personal');
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Personal Information'),
-        backgroundColor: Colors.purple,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _weightController,
-                decoration: const InputDecoration(
-                  labelText: 'Weight (lbs)',
-
         appBar: AppBar(
           title: const Text('Personal Information'),
           backgroundColor: Colors.purple,
@@ -117,90 +90,95 @@ final authUser = FirebaseAuth.instance.currentUser;
                     }
                     return null;
                   },
-
                 ),
-                //initialValue: fieldValue,
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter> [
-                 FilteringTextInputFormatter.digitsOnly
-  ],
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your weight';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                controller: _heightController,
-                decoration: const InputDecoration(
-                  labelText: 'Height (inch)',
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _heightController,
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Height (inch)',
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                  ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your height';
+                    }
+                    return null;
+                  },
                 ),
-               // initialValue: ,
-                keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter> [
-                 FilteringTextInputFormatter.digitsOnly
-  ],
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your height';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _calculateBMI();
-                  }
-                },
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _calculateBMI();
+                    }
+                  },
+                  // ignore: prefer_const_constructors
+                  child: Text('Calculate BMI'),
+                ),
                 // ignore: prefer_const_constructors
-                child: Text('Calculate BMI'),
-              ),
-              // ignore: prefer_const_constructors
-              SizedBox(height: 16.0),
-              Text(
-                bmiResult == 0.0
-                    ? 'Please enter your weight and height'
-                    : 'Your BMI is ${bmiResult.toStringAsFixed(1)}',
-
-                // ignore: prefer_const_constructors
-
-                style: TextStyle(fontSize: 20.0),
-              ),
-              TextButton(
-                child: const Text(
-                    //User presses this button to submit valid information
-                    'SUBMIT'),
-                onPressed: () {
-                   const snackbar = SnackBar(
-                    content: Text("Information Changed"),
-                    backgroundColor: Colors.green,
+                SizedBox(height: 16.0),
+                Text(
+                  bmiResult == 0.0
+                      ? 'Please enter your weight and height'
+                      : 'Your BMI is ${bmiResult.toStringAsFixed(1)}',
+                  // ignore: prefer_const_constructors
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                TextButton(
+                  child: const Text(
+                      //User presses this button to submit valid information
+                      'SUBMIT'),
+                  onPressed: () {
+                    FirebaseFirestore.instance
+                        .collection('personal')
+                        .doc(authUser?.uid)
+                        .set({
+                          'weight': _weightController.text,
+                          'height': _heightController.text,
+                          'bmi': bmiResult
+                        })
+                        .then((value) => print("added"))
+                        .catchError((error) => print("Failed to add: $error"));
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          //The right side is the widget you want to go to
+                          builder: (context) => LoginScreen()),
                     );
-                  FirebaseFirestore.instance
-                      .collection('personal')
-                      .doc(authUser?.uid)
-                      .set({
-                        'user' : authUser?.uid,
-                        'weight': _weightController.text,
-                        'height': _heightController.text,
-                        'bmi': bmiResult
-                      })
-                      .then((value) => print("added"))
-                      .catchError((error) => print("Failed to add: $error"));
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        //The right side is the widget you want to go to
-                        builder: (context) => WorkoutPage()),
-                  );
-                },
-              )
-            ],
+                  },
+                ),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('personal')
+                        .doc(authUser?.uid)
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<DocumentSnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      }
+                      if (!snapshot.hasData) {
+                        return Text('Document does not exist');
+                      }
+                      Map<String, dynamic> data =
+                          snapshot.data!.data() as Map<String, dynamic>;
+                      return Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('Height: ${data['height']}'),
+                            Text('Weight: ${data['weight']}'),
+                            Text('BMI: ${data['bmi']}'),
+                          ]);
+                    })
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
