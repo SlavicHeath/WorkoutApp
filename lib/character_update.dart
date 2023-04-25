79dd6ecbf1ce72ac21694042a43aa3fe76b37ac6
Slavic Heath <mvh2069@uncw.edu>
2023-04-25 10:36:38 -0400

    Fix the merge errors

--- lib/character_update.dart
+++ lib/character_update.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 import 'package:workoutpet/battle.dart';
 import 'package:workoutpet/workout.dart';
@@ -40,57 +43,5 @@ _updateModel() {
     return displayFile2;
   } else if ((xp >= 600) && (xp < 1000)) {
     return displayFile3;
-  }
-}
-
-class PeopleList extends StatelessWidget {
-  PeopleList({super.key});
-
-  // Reference to the Firestore "People" collection
-  final modelRef = FirebaseFirestore.instance
-      .collection('Character')
-      .where(documentId, isEqualTo: authUser!.uid)
-      .snapshots();
-
-  var name;
-  Future getName() async {
-    DocumentSnapshot ds = await FirebaseFirestore.instance
-        .collection('character')
-        .doc(authUser!.uid)
-        .get();
-    name = ds.data();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: modelRef, //.snapshots() gives us a Stream
-      builder: (context, snapshot) {
-        // Make sure that the snapshot has data with it.
-        // There may be no data while the network connection is initializing.
-        // And sometimes the data is empty, like and empty street.
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          print(name);
-          return const Text("No data to show!");
-        }
-
-        // Here is the list of Documents from the People collection.
-        var modeldoc = snapshot.data!.docs.map((e) => null);
-        // Use a ListView.builder to generate a ListView
-        // to display the People collection
-
-        return ListView.builder(
-          itemCount: modeldoc.length,
-          itemBuilder: ((context, index) {
-            return Text('${modeldoc.first}');
-          }),
-        );
-        // return TextFormField(
-        //   decoration: const InputDecoration(
-        //       border: UnderlineInputBorder(),
-        //       labelText: "${modeldoc.get('Character')}"),
-        // );
-      },
-    );
   }
 }
